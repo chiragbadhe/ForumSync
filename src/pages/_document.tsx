d98a0e77d96cf2b2ddf41c51161173bf09ed69d4
chiragbadhe <badhechirag633@gmail.com>
2023-03-24 17:51:04 +0530

    fix: dynamic head element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/styles.css" />
+        <title>ForumSync</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
