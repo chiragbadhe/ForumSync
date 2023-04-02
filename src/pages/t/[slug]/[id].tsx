22ea164550cfe5db13214690f4a1c07c0cf8b21e
chiragbadhe <badhechirag633@gmail.com>
2023-04-02 16:41:42 +0530

    ft: user_avatar
    Co-authored-by: chetan.eth 0xchetan

--- src/pages/t/[slug]/[id].tsx
+++ src/pages/t/[slug]/[id].tsx
@@ -24,9 +24,9 @@ const TopicPage: React.FC = () => {
   const router = useRouter();
   const { slug, id } = router.query;
   const [topicData, setTopicData] = useState<TopicData>();
-  const { protocol, setProtocol } = useProtocolStore();
+  const { protocol } = useProtocolStore();
 
-  console.log(topicData?.post_stream?.posts[0].cooked);
+  console.log(protocol.forumLink);
 
   useEffect(() => {
     const fetchTopicData = async () => {
@@ -51,17 +51,22 @@ const TopicPage: React.FC = () => {
         {topicData?.post_stream?.posts.map((post) => (
           <div className="relative container mx-auto max-w-3xl ">
             <div className="absolute flex space-x-[15px] -left-[60px]  rounded-[5px]">
-              <img
-                className="h-[50px] rounded-full border"
-                src="https://sea2.discourse-cdn.com/business6/user_avatar/gov.uniswap.org/ashleigh/90/10_2.png"
-                alt=""
-              />
-              {/* <div>
-            <p>{post.username}</p>
-           </div> */}
+              {post.avatar_template.match(/letter/) ? (
+                <img
+                  className="h-[50px] rounded-full border"
+                  src={`${post.avatar_template.replace('{size}', '90')}`}
+                  alt=""
+                />
+              ) : (
+                <img
+                  className="h-[50px] rounded-full border"
+                  src={`${protocol.forumLink}${post.avatar_template.replace('{size}', '90')}`}
+                  alt=""
+                />
+              )}
             </div>
             <div className="mt-[25px] bg-gray-100 rounded-[10px] p-[20px] border" key={post.id}>
-              <div className='flex justify-between '>
+              <div className="flex justify-between ">
                 <p className="font-semibold text-[16px] mb-[15px]">{post.username}</p>
                 <p className="opacity-50 text-[14px] mb-[15px]">{post.created_at}</p>
               </div>
